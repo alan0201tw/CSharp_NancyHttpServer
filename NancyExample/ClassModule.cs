9fff01cd1fd770e454e0d7cb9f734254ed63801b
DESKTOP-6J14KSJ\AlanShih <alan0201tw@gmail.com>
2017-05-19 22:20:30 +0800

    initial

---
+++ NancyExample/ClassModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace NancyExample
+{
+    public class ClassModule : NancyModule
+    {
+        public ClassModule()
+        {
+            Get["/"] = _ => "Hello World!";
+            Get["/status"] = _ => "Hello World! I'm at subdirectory - status!";
+
+            Get["/class/{id}"] = parameters =>
+            {
+                int ID = parameters.id;
+
+                //return Negotiate.WithStatusCode(HttpStatusCode.OK).WithModel(ID);
+                return "Yeeeeee" + ID.ToString();
+            };
+
+        }
+    }
+}
